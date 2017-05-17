d00f0662af3b6e2de01e35ca1cc3ed14cb5f07cf
arth-complitech <arth@complitech.net>
2017-05-17 16:06:59 +0530

    Franchise Leader added

---
+++ app/controllers/api/franchise_leaders_controller.rb
@@ -0,0 +1,16 @@
+class Api::FranchiseLeadersController < ApplicationController
+	def create
+		area = Area.find(params[:area_id])
+		@franchise_leader = area.franchise_leaders.build(name: params[:name], birth_date: params[:birth_date], mobile_number: params[:mobile_number], exp_in_cab_business: params[:exp_in_cab_business], monthly_expectations: params[:monthly_expectations])
+		if @franchise_leader.save
+			render_json({:result=>{:messages =>"ok",:rstatus=>1, :errorcode =>""},:data=>{:messages =>"Registered Successfully!"}}.to_json)
+		else
+			render_json({:result=>{:messages =>"ok",:rstatus=>0, :errorcode =>"401"},:data=>{:messages =>"Something went wrong!"}}.to_json)
+		end
+	end
+
+	private
+	def format_dates
+    params[:birth_date] = DateTime.strptime(params[:birth_date], "%m-%d-%Y") if params[:birth_date].present?
+  end
+end
