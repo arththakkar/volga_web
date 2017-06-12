5dbe513fe3e82824856f5793bceaf9e8aa64fe0b
Arth T <arththakkar@gmail.com>
2017-06-12 20:28:46 +0530

    Date

--- app/controllers/franchise_leaders_controller.rb
+++ app/controllers/franchise_leaders_controller.rb
@@ -1,5 +1,6 @@
 class FranchiseLeadersController < ApplicationController
 	def index
+		date = Date.parse("2017/06/07")
 		@franchise_leaders = FranchiseLeader.where('created_at > ?', date.midnight)
 	end
 end
--- app/controllers/franchises_controller.rb
+++ app/controllers/franchises_controller.rb
@@ -1,5 +1,6 @@
 class FranchisesController < ApplicationController
 	def index
+		date = Date.parse("2017/06/07")
 		@franchises = Franchise.where('created_at > ?', date.midnight)
 	end
 end
