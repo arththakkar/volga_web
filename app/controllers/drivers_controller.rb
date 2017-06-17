9f74d494286e3cc086693e2cf6da747720f530c6
Arth T <arththakkar@gmail.com>
2017-06-18 01:21:55 +0530

    data

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -1,6 +1,6 @@
 class DriversController < ApplicationController
 	def index
-		date = Date.parse("2017/06/12")
+		date = Date.parse("2017/06/07")
 		@drivers = Driver.where('created_at > ?', date.midnight)
 	end
 end
