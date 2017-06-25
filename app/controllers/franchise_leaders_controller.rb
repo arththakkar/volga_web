ee7103bde74f2f990badcbf2a50189606ceb07a8
Arth T <arththakkar@gmail.com>
2017-06-26 00:15:27 +0530

    Driver list 17th june

--- app/controllers/franchise_leaders_controller.rb
+++ app/controllers/franchise_leaders_controller.rb
@@ -1,6 +1,6 @@
 class FranchiseLeadersController < ApplicationController
 	def index
-		date = Date.parse("2017/06/12")
+		date = Date.parse("2017/06/17")
 		@franchise_leaders = FranchiseLeader.where('created_at > ?', date.midnight)
 	end
 end
