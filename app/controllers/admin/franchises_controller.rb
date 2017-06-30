d9f47bf41d666627be2e295933078a65f0a6d5f0
Arth T <arththakkar@gmail.com>
2017-07-01 03:39:35 +0530

    Mobile Number added

--- app/controllers/admin/franchises_controller.rb
+++ app/controllers/admin/franchises_controller.rb
@@ -19,7 +19,7 @@ class Admin::FranchisesController < ApplicationController
 
 	private
 	def candidate_params
-		params.require(:confirm_franchise).permit(:name, :address1, :address2, :city, :state, :country, :birthdate, :pan_no, :email_id, :adhaar_no, :nominee_name, :relation_with_nominee, :amount, :cheque_no, :cheque_date, :bank_name, :is_pan_card, :is_adhaar_card, :is_light_bill)
+		params.require(:confirm_franchise).permit(:name, :address1, :address2, :city, :state, :country, :birthdate, :pan_no, :email_id, :adhaar_no, :nominee_name, :relation_with_nominee, :amount, :cheque_no, :cheque_date, :bank_name, :is_pan_card, :is_adhaar_card, :is_light_bill, :mobile_no)
 	end
 
 	def formate_dates
