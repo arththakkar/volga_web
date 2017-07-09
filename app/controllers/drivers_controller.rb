76a41ad55dd143031ff7f317e5090221a2f6f534
Karan29 <karan@complitech.net>
2017-07-09 22:54:38 +0530

    adding driver details

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -1,6 +1,13 @@
 class DriversController < ApplicationController
 	def index
-		date = Date.parse("2017/06/17")
-		@drivers = Driver.where('created_at > ?', date.midnight)
+		@drivers = Driver.all
+	end
+
+	def export_driver
+		@drivers = Driver.all
+		respond_to do |format|
+			format.html
+			format.xls
+		end
 	end
 end
