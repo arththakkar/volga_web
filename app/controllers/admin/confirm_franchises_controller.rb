a38dc0305bcf9ee91172b9a010056a56a9df103a
Arth T <arththakkar@gmail.com>
2017-08-01 23:16:15 +0530

    delete confirm franchise

--- app/controllers/admin/confirm_franchises_controller.rb
+++ app/controllers/admin/confirm_franchises_controller.rb
@@ -58,6 +58,12 @@ class Admin::ConfirmFranchisesController < ApplicationController
 		end
 	end
 
+	def destroy
+		franchise = ConfirmFranchise.find(params[:id])
+		franchise.destroy
+		redirect_to admin_confirm_franchises_path
+	end
+
 	private
 	def candidate_params
 		params.require(:confirm_franchise).permit(:name, :address1, :address2, :city, :state, :country, :birthdate, :pan_no, :email_id, :adhaar_no, :nominee_name, :relation_with_nominee, :amount, :cheque_no, :cheque_date, :bank_name, :is_pan_card, :is_adhaar_card, :is_light_bill, :mobile_no)
